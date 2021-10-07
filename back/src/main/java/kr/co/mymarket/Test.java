feb163de869153ad86ca84bbcea11fe30b0e7a4f
wonchangPark <parkwc0213@naver.com>
2021-10-07 12:27:51 +0900

    add test

--- back/src/main/java/kr/co/mymarket/Test.java
+++ back/src/main/java/kr/co/mymarket/Test.java
@@ -2,5 +2,5 @@ package kr.co.mymarket;
 
 public class Test {
     // dev_back 에서 새로운 브랜치 생성(js_hotfix) 뒤 작업 후 pull request 테스트
-    // dev_back 에서 새로운 브랜치 생성(wonchang) java 11
+    // dev_back 에서 새로운 브랜치 생성(wonchang) java 11 add
 }
